ff3d85b42264045fcf31820abdff2f4eec10ea21
Aaron Rafter <raftera@oregonstate.edu>
2021-05-29 14:46:52 -0700

    Implemented method for leap years in multiple of 400

--- leapyear.py
+++ leapyear.py
@@ -1,6 +1,9 @@
 def leapyear(year):
     if (year % 4 == 0):
         if (year % 100 == 0):
-            return False
+            if (year % 400 == 0):
+                return True
+            else:
+                return False
         else:
          return True

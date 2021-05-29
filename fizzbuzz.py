f2af3da9054d360d2ca54561c766ea0471c3292b
Aaron Rafter <raftera@oregonstate.edu>
2021-05-29 14:37:04 -0700

    Implemented method to throw error if num is out of 1 to 100 range

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,4 +1,6 @@
 def fizzbuzz(int):
+    if (int < 1 or int > 100):
+        raise ValueError('Int must be between 1 and 100')
     if (int % 5 == 0 and int % 3 == 0):
         return "FizzBuzz"
     if (int % 5 == 0):

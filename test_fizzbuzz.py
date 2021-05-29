4cbeefd008a8bf8cec4580f81dd3991ff160d558
Aaron Rafter <raftera@oregonstate.edu>
2021-05-29 14:30:29 -0700

    wrote test for non fizz/buzz output

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -7,6 +7,9 @@ class TestCase(unittest.TestCase):
     def test_fizzbuzz1(self):
         result = fizzbuzz.fizzbuzz(5)
         self.assertEqual(result, "Buzz")
+    def test_fizzbuzz2(self):
+        result = fizzbuzz.fizzbuzz(2)
+        self.assertEqual(result, 2)
 
 
 if __name__ == '__main__':

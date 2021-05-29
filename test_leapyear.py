8cb44cca5c3093605f715f2cbeb215314bfb7a74
Aaron Rafter <raftera@oregonstate.edu>
2021-05-29 14:46:05 -0700

    Implemented test for leap year in multiple of 400

--- test_leapyear.py
+++ test_leapyear.py
@@ -8,6 +8,9 @@ class TestCase(unittest.TestCase):
     def test_leapyear2(self):
         result = leapyear.leapyear(100)
         self.assertEqual(result, False)
+    def test_leapyear3(self):
+        result = leapyear.leapyear(400)
+        self.assertEqual(result, True)
 
 
 if __name__ == '__main__':

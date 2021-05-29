498a0be1723669b1f95a0f61e0d6bda82c1a7459
Aaron Rafter <raftera@oregonstate.edu>
2021-05-29 14:44:25 -0700

    Wrote test to test leap year multiples of 100

--- test_leapyear.py
+++ test_leapyear.py
@@ -5,6 +5,9 @@ class TestCase(unittest.TestCase):
     def test_leapyear1(self):
         result = leapyear.leapyear(4)
         self.assertEqual(result, True)
+    def test_leapyear2(self):
+        result = leapyear.leapyear(100)
+        self.assertEqual(result, False)
 
 
 if __name__ == '__main__':

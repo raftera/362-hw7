9b5d8d619f0911c64828ee05d1ea2f3f859122ac
Aaron Rafter <raftera@oregonstate.edu>
2021-05-29 14:42:16 -0700

    Wrote first test, tests for normal leap year (divisible by 4)

---
+++ test_leapyear.py
@@ -0,0 +1,11 @@
+import unittest
+import test_leapyear
+
+class TestCase(unittest.TestCase):
+    def test_leapyear1(self):
+        result = leapyear.leapyear(4)
+        self.assertEqual(result, True)
+
+
+if __name__ == '__main__':
+    unittest.main()

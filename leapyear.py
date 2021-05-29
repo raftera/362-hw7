2119cb96403183779e8646c878d86a36a15184b1
Aaron Rafter <raftera@oregonstate.edu>
2021-05-29 14:52:26 -0700

    Added method to raise ValueError if input is not an integer

--- leapyear.py
+++ leapyear.py
@@ -1,4 +1,6 @@
 def leapyear(year):
+    if (not(isinstance(year, int))):
+        raise ValueError('Please enter an integer')
     if (year % 4 == 0):
         if (year % 100 == 0):
             if (year % 400 == 0):
